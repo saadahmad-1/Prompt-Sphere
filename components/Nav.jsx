541a735b3e8841380da595bd5164efc9825e83fa
Saad Ahmad <saadahmad.cs@gmail.com>
2023-12-23 01:55:54 +0500

    second commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -29,7 +29,7 @@ const Nav = () => {
           height={30}
           className="object-contain"
         />
-        <p className="logo_text">PromptSphere</p>
+        <p className="logo_text">PromptUp</p>
       </Link> 
 
       {/* Desktop Nav */}
